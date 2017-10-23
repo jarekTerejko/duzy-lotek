c5548daa3765467fdff44e092d77c5198931cff7
Jarosław Terejko <j.terejko@gmail.com>
2017-10-23 16:40:08 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+(function () {
+    var btn = document.querySelector('#getNumbers'),
+        output = document.querySelector('#showNumbers');
+
+    function getRandom(min, max) {
+
+        return Math.round(Math.random() * (max - min) + min);
+
+    }
+
+    function showRandomNumbers() {
+
+        var numbers = [];
+
+        for (var i = 0; i < 6; i++) {
+            random = getRandom(1, 49);
+
+            while (numbers.indexOf(random) !== -1) {
+                random = getRandom(1, 49);
+                console.log('Liczby wylosowanie wielokrotnie to:' + random);
+            }
+
+            numbers.push(random);
+
+        }
+
+        output.value = numbers.join(', ');
+
+    }
+
+    btn.onclick = showRandomNumbers;
+
+})();
